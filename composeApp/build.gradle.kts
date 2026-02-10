de8f9c3b2f985b7207c8789c02b4a35c7f3659a2
w-jablonski <wojablonski@gmail.com>
2026-02-10 21:56:46 +0000

    update deps

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
-    alias(libs.plugins.composeHotReload)
+    // alias(libs.plugins.composeHotReload)
     alias(libs.plugins.sqldelight)
 }
 
